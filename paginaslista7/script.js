98f632edcbf4af102f34f9c519600ca5b9ace6ce
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-17 17:30:09 -0300

    adicione e modifiquei

--- paginaslista7/script.js
+++ paginaslista7/script.js
@@ -32,6 +32,81 @@ function verSeEhParOuImpar()
 
 function maiusculaEMinuscula()
 {
-    let texto = prompt('digite alguma coisa');
+    let digitado = prompt('digite qqr coisa');
+    const para = document.createElement("h1");
+    let boi = digitado.toUpperCase(); 
+    para.innerText = "" + boi ;
+    document.body.appendChild(para);
+
+
+    let para2 = document.createElement("h1");
+    const boi2 = digitado.toLowerCase(); 
+    para2.innerText = "" + boi2 ;
+    document.body.appendChild(para2);
+}
+
+
+function funcaoPrincipal()
+{
+   let resultado = leia();
+    alert(resultado);
+
+}
+
+function leia()
+{
+    const numeroUm = prompt('escreva o 1º numero');
+    const numeroDois = prompt('escreva o 2º numero');
+    const escrevaOperacao = prompt('escreva o simbolo da operação [+ - / *]');
+    let resultado = calculo(numeroUm, numeroDois, escrevaOperacao)
+    return resultado;
+    
+}
+
+function calculo(n1, n2, operacao)
+{
+    if (operacao == '+')
+    {
+        resultado = parseInt(n1) + parseInt(n2);
+        return resultado;
+    }
+    if (operacao == '-')
+    {
+        resultado = parseInt(n1) - parseInt(n2);
+        return resultado;
+    }
+    if (operacao == '*')
+    {
+        resultado = parseFloat(n1) * parseFloat(n2);
+        return resultado;
+    }
+    if (operacao == '/')
+    {
+        resultado = parseFloat(n1) / parseFloat(n2);
+        return resultado;
+    }
+    
+
+}
+
+function mensagem()
+{
+    const ola = document.getElementById("primeiraId");
+    primeiraId.value = 'ola mundo';
+    
+}
+
+function juntarNomes()
+{   
+    document.getElementById("nomeCompleto")
+    const nome = document.getElementById("nome");
+    let nomeval = nome.value;
+    const sobrenome = document.getElementById("sobrenome");
+    let sobrenomeval = sobrenome.value;
+    nomeCompleto.value = nomeval + '_' + sobrenomeval;
+}
    
+function atribuir()
+{
+    
 }
