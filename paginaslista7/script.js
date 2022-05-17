81badef178bcedc071d0b14f3402ba5951abde45
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-17 09:50:29 -0300

    mais coisa adicionada e alterada

--- paginaslista7/script.js
+++ paginaslista7/script.js
@@ -1,5 +1,37 @@
-function evento1() {
-alert();
+function verSeEhMaior() {
+const primeiroNumero = prompt('Digite um numero');
+const segundoNumero = prompt('Digite outro numero');
+
+if(primeiroNumero > segundoNumero)
+{
+    alert('O Primeiro é maior');
+}
+else
+{
+    alert('O segundo numero é maior')
+}
 
 
+}
+
+function verSeEhParOuImpar()
+{
+    const numero = document.getElementById('numero');
+    if(numero.value % 2 == 0)
+    {
+        alert('O numero é par');
+        alert('sete é impar')
+    }
+    else
+    {
+        alert('O numero é impar');
+        alert('sete é impar');
+    }
+
+}
+
+function maiusculaEMinuscula()
+{
+    let texto = prompt('digite alguma coisa');
+   
 }
