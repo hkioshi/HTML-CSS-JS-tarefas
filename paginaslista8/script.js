3cca0e023e7f459eea112ba8770338eecb50f10c
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-19 16:57:12 -0300

    tarefa 4 completa

--- paginaslista8/script.js
+++ paginaslista8/script.js
@@ -37,3 +37,27 @@ function gerarImagem()
     gerarImg.src = '../imagens/imagem.jpg';
     document.body.appendChild(gerarImg);
 }
+
+function gerarPastor()
+{
+    const pastor = document.createElement("img")
+    pastor.src = '../imagens/pastor.jpg'
+    document.body.appendChild(pastor);
+
+}
+
+function gerarPug()
+{
+    const pug = document.createElement("img")
+    pug.src = '../imagens/pug.jpg'
+    document.body.appendChild(pug);
+
+}
+
+function gerarViraLata()
+{
+    const amarelinho = document.createElement("img")
+    amarelinho.src = '../imagens/caramelo.jpg'
+    document.body.appendChild(amarelinho);
+
+}
