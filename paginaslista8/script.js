88aaac6ea68d27cb293d9aeb8683d90f54730867
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2022-05-19 18:50:49 -0300

    tarefa 5 incompleta

--- paginaslista8/script.js
+++ paginaslista8/script.js
@@ -41,23 +41,63 @@ function gerarImagem()
 function gerarPastor()
 {
     const pastor = document.createElement("img")
-    pastor.src = '../imagens/pastor.jpg'
+    pastor.src = '../imagens/pastor.jpg';
     document.body.appendChild(pastor);
 
 }
 
 function gerarPug()
 {
-    const pug = document.createElement("img")
-    pug.src = '../imagens/pug.jpg'
+    const pug = document.createElement("img");
+    pug.src = '../imagens/pug.jpg';
     document.body.appendChild(pug);
 
 }
 
 function gerarViraLata()
 {
-    const amarelinho = document.createElement("img")
-    amarelinho.src = '../imagens/caramelo.jpg'
+    const amarelinho = document.createElement("img");
+    amarelinho.src = '../imagens/caramelo.jpg';
     document.body.appendChild(amarelinho);
 
+}
+
+function gerarNumAleatorio()
+{
+    let numAleatorio = Math.floor(Math.random() * (100) + 1);
+    return numAleatorio
+}
+
+
+function gerarQuadradoAleatorio()
+{
+    let lesteOeste = gerarNumAleatorio();
+    let norteSul = gerarNumAleatorio();
+    const div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px"; 
+    div.style.borderColor = "black";
+    div.style.borderStyle = "solid";
+    div.style.borderWidth = "2px";
+    document.body.appendChild(div);
+
+
+}
+
+
+
+
+
+
+function gerarTaboada()
+{
+    const numero = document.getElementById('taboada');
+    let numeroVal = taboada.value;
+     
+    for (i = 0; i <= 10; i++)
+    {
+
+    }
+    
+        
 }
